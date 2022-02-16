6709e0b14525876f032dfb0cdc02b7db08c41f23
HARRY OMOLLOH <harryomollo95@gmail.com>
2022-02-16 01:17:14 -0800

    How we work db models

--- how_we_work/admin.py
+++ how_we_work/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
-
+from .models import HowWeWork, Faq
 # Register your models here.
+
+admin.site.register(HowWeWork)
+admin.site.register(Faq)
--- how_we_work/models.py
+++ how_we_work/models.py
@@ -1,3 +1,37 @@
 from django.db import models
 
+def save_one_only(model_name):
+    def save(self, *args, **kwargs):
+        if not self.pk and model_name.objects.exists():
+        # if you'll not check for self.pk 
+        # then error will also raised in update of exists model
+            raise ValidationError('There is can be only one '+model_name+' instance')
+        return super(model_name, self).save(*args, **kwargs)
+
 # Create your models here.
+class HowWeWork(models.Model):
+    
+    introduction = models.TextField()
+    body = models.TextField(blank=True,null=True)
+    date = models.DateField(auto_now_add=True)
+    thumbnail = models.FileField(blank=True,null=True)
+
+    save_one_only('HowWeWork')
+
+    def __str__(self):
+        return self.introduction[:50]
+
+class Faq(models.Model):
+
+    question = models.CharField(max_length=256)
+    answer = models.TextField()
+
+    def __str__(self):
+        return self.question
+
+class HowWeWorkCheckList(models.Model):
+    name = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.name
+    
