5507228a52166a3c8403f68f7284b312eaa7c61e
HARRY OMOLLOH <harryomollo95@gmail.com>
2022-04-06 16:09:55 -0700

    Added domain names

--- studyhelpapp/settings.py
+++ studyhelpapp/settings.py
@@ -15,7 +15,7 @@ SECRET_KEY = ')s@gc4pqkyjtgw2^^d&l%$s+w4f^9r^345r&qkjbtx9+8e9)%t'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['studyhelpapp.herokuapp.com','*']
 
 
 # Application definition
