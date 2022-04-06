ea12682b7eaaa157b0f448ebf0bfd14332313f56
HARRY OMOLLOH <harryomollo95@gmail.com>
2022-04-06 16:07:37 -0700

    Added prodb

--- studyhelpapp/settings.py
+++ studyhelpapp/settings.py
@@ -164,3 +164,5 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 #  Add configuration for static files storage using whitenoise
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
+prod_db  =  dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(prod_db)
