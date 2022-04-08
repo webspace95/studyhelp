a93082d3e23ac5a950fb9d90d787b2809d346648
HARRY OMOLLOH <harryomollo95@gmail.com>
2022-04-08 16:14:11 -0700

    created a signal for an invalid ipn

--- payments/signals.py
+++ payments/signals.py
@@ -28,3 +28,25 @@ def payment_notification(sender, **kwargs):
                 amount=order.price
             )
             payment.save()
+
+@receiver(invalid_ipn_received)
+def payment_notification_two(sender, **kwargs):
+    ipn = sender
+    if ipn.payment_status == 'Completed':
+        # payment was successful
+        order = get_object_or_404(Order, id=ipn.invoice)
+
+        if order.price == ipn.mc_gross:
+            # mark the order as paid
+            order.payment_complete = 'T'
+            order.save()
+
+            charge = create_charge_id()
+            charge = charge.upper()
+
+            payment = Payment(
+                charge_id=charge,
+                user=order.user,
+                amount=order.price
+            )
+            payment.save()
