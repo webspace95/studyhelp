e6372ca1077aa85f1a2cbbffe6c0e84f29aa53f5
HARRY OMOLLOH <harryomollo95@gmail.com>
2022-04-08 15:37:22 -0700

    finished ipn signals

--- payments/apps.py
+++ payments/apps.py
@@ -3,3 +3,7 @@ from django.apps import AppConfig
 
 class PaymentsConfig(AppConfig):
     name = 'payments'
+
+    def ready(self):
+        # import signal handlers
+        import payments.signals
