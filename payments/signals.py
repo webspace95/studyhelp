ecca67066d62575f7a5e5a233283e0c41dfd011c
HARRY OMOLLOH <harryomollo95@gmail.com>
2022-04-08 16:53:48 -0700

    Fixed order query statement

--- payments/signals.py
+++ payments/signals.py
@@ -1,7 +1,7 @@
 from django.shortcuts import get_object_or_404
 from .models import Payment, Address
 from jobs.models import Order
-from paypal.standard.ipn.signals import valid_ipn_received,invalid_ipn_received
+from paypal.standard.ipn.signals import valid_ipn_received
 from django.dispatch import receiver
 
 def create_charge_id():
@@ -12,29 +12,7 @@ def payment_notification(sender, **kwargs):
     ipn = sender
     if ipn.payment_status == 'Completed':
         # payment was successful
-        order = get_object_or_404(Order, reference_code=ipn.invoice)
-
-        if order.price == ipn.mc_gross:
-            # mark the order as paid
-            order.payment_complete = 'T'
-            order.save()
-
-            charge = create_charge_id()
-            charge = charge.upper()
-
-            payment = Payment(
-                charge_id=charge,
-                user=order.user,
-                amount=order.price
-            )
-            payment.save()
-
-@receiver(invalid_ipn_received)
-def payment_notification_two(sender, **kwargs):
-    ipn = sender
-    if ipn.payment_status == 'Completed':
-        # payment was successful
-        order = get_object_or_404(Order, reference_code=ipn.invoice)
+        order = Order.objects.get(reference_code=ipn.invoice)
 
         if order.price == ipn.mc_gross:
             # mark the order as paid
