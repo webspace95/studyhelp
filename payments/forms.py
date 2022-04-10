c695cc972c657b3ae6aef752247b43a72a4d97ad
HARRY OMOLLOH <harryomollo95@gmail.com>
2022-04-10 05:32:02 -0700

    Completed payments form of paypal lets see

--- payments/forms.py
+++ payments/forms.py
@@ -1,8 +1,17 @@
 from django import forms
 from django.forms import MultipleChoiceField, ChoiceField
+from paypal.standard.forms import PayPalPaymentsForm
 
 class CheckoutForm(forms.Form):
     billing_address = forms.CharField(required=False)
     billing_address2 = forms.CharField(required=False)
     billing_zip = forms.CharField(required=False)
 
+
+class MyPayPalPaymentsForm(PayPalPaymentsForm):
+    def render(self):
+        form_open  = u'''<form action="%s" method="post">''' % (self.get_endpoint())
+        form_close = u'</form>'
+        # format html as you need
+        submit_elm = u'''<input type="submit" value="Complete payment" class="button fit">'''
+        return format_html(form_open+self.as_p()+submit_elm+form_close)
