ce7686cf4e637ee20f70be04c834676975d12ba8
HARRY OMOLLOH <harryomollo95@gmail.com>
2022-04-22 02:50:05 -0700

    fixed django countries for the last time I think?

--- payments/forms.py
+++ payments/forms.py
@@ -2,7 +2,7 @@ from django import forms
 from django.forms import MultipleChoiceField, ChoiceField
 from paypal.standard.forms import PayPalPaymentsForm
 from django.utils.html import format_html
-from django_countries.widgets import CountrySelectWidget
+
 
 class CheckoutForm(forms.Form):
     billing_address = forms.CharField(required=False)
