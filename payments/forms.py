a52273105a2395809a12d518a02cf63b75b9f3e4
HARRY OMOLLOH <harryomollo95@gmail.com>
2022-04-22 02:44:30 -0700

    fixed django heroku problem

--- payments/forms.py
+++ payments/forms.py
@@ -2,7 +2,6 @@ from django import forms
 from django.forms import MultipleChoiceField, ChoiceField
 from paypal.standard.forms import PayPalPaymentsForm
 from django.utils.html import format_html
-from django_countries.fields import CountryField
 from django_countries.widgets import CountrySelectWidget
 
 class CheckoutForm(forms.Form):
